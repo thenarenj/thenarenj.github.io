8372416ba1535f26148defef2f675183d06024b7
iamrf <rf.fa17@gmail.com>
2023-12-10 19:35:59 +0330

    daisy-ui initialized

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
       نارنج
+      <button className="btn btn-primary">لینک</button>
     </main>
   )
 }
