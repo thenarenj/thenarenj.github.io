b5537b712dd0d3a7b70f6e72fd6b11126925684a
iamrf <rf.fa17@gmail.com>
2023-12-16 18:00:30 +0330

    navbar link changed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
     return (
         <nav className="absolute top-0 right-0 left-0 z-10 pt-4 px-4">
             <div className='container mx-auto flex items-center justify-center'>
-                <a href="#home">
+                <Link href="#home">
                     <Image
                         src="/assets/images/logotypefa.png"
                         alt="Logo"
@@ -12,7 +13,7 @@ const Navbar = () => {
                         height={70}
                         priority
                     />
-                </a>
+                </Link>
             </div>
         </nav>
     )
